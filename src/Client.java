414e4c76fd3b9b90f49d4b78995e5fee96367179
kullph <kullaphat01.tr@gmail.com>
2023-05-24 16:15:33 +0700

    socket room logic and game logic finish with no GUI

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,23 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import javax.swing.*;
 
-public class Client {
+public class Client extends JFrame {
+        
     public static void main(String[] args) throws IOException {
+
+        Random random = new Random();
         String host = "localhost";
         int lobby = 10000;
 
-        Socket socket = new Socket(host, lobby);
-        System.out.println("Connected to server " + host + ":" + lobby);
+        
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        Socket lobbySocket = new Socket(host, lobby);
+        System.out.println("Connected to " + host + ":" + lobby);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(lobbySocket.getInputStream()));
+        PrintWriter out = new PrintWriter(lobbySocket.getOutputStream(), true);
 
         BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
         String userInput;
@@ -20,13 +27,324 @@ public class Client {
             if (userInput.equals("exit")) {
                 break;
             }
+            //JOIN 41234
+            else if(userInput.startsWith("JOIN")){
+                int score = 0;
+                System.out.println("joincmd");
+                String[] commandParts = userInput.split(" ");
+                int port = Integer.parseInt(commandParts[1]);
+                Socket game = new Socket(host, port);
+                BufferedReader gameIn = new BufferedReader(new InputStreamReader(game.getInputStream()));
+                PrintWriter gameOut = new PrintWriter(game.getOutputStream(), true);
+
+                BufferedReader gameStdIn = new BufferedReader(new InputStreamReader(System.in));
+                String gameUserInput;
+                //CALL GUI
+                while (true) {
+
+                    gameUserInput = gameStdIn.readLine();
+                    if (gameUserInput.equals("exit")) {
+                        break;
+                    }
+                    else if(gameUserInput.equals("start")){
+                        score += random.nextInt(11) + 1;
+                        score += random.nextInt(11) + 1;
+                        System.out.println(score);
+                        if(score == 21){
+                            gameOut.println("SCOREX 21");
+                        }
+                        else if(score > 21){
+                            gameOut.println("SCORE 0");
+                        }
+                    }
+                    else if(gameUserInput.equals("more")){
+                        score += random.nextInt(11) + 1;
+                        System.out.println(score);
+                    }
+                    else if(gameUserInput.equals("fold")){
+                        if(score > 21){
+                            gameOut.println("SCORE 0");    
+                        }
+                        else{
+                            gameOut.println("SCORE " + score);
+                        }
+                        
+                    }
+
+                    gameOut.println(gameUserInput);
+                    System.out.println(gameIn.readLine());
+                }
+                
+            }
             out.println(userInput);
+            
             System.out.println(in.readLine());
+            
         }
 
         in.close();
         out.close();
         stdIn.close();
-        socket.close();
+        lobbySocket.close();
     }
 }
+
+
+
+// import java.io.*;
+// import java.net.*;
+// import java.util.Random;
+// import javax.swing.*;
+// import java.awt.*;
+// import java.awt.event.ActionEvent;
+// import java.awt.event.ActionListener;
+
+// public class Client extends JFrame implements ActionListener{
+//     private static PrintWriter out;
+//     private static BufferedReader in;
+//     private static PrintWriter Rout;
+//     private static BufferedReader Rin;
+//     private static String host = "localhost";
+//     private static int lobby = 10000;
+//     private Socket game;
+//     private static Socket lobbySocket;
+//     private int score = 0;
+//     private static boolean x = false;
+//     private int GameRoom;
+//     private static int port;
+    
+
+//     //Room
+//     private String gameStatus = "Status Playing";
+//     private JPanel lobbyPanelRoom;
+//     private JButton Draw;
+//     private JButton Fold;
+//     private JButton Start;
+//     private JButton Ghost;
+//     private JLabel TittleStatus;
+//     private JLabel StatusLabel;
+//     private JLabel TittlePoint;
+//     private JLabel TotalPoint;
+
+//     //Lobby
+//     private JPanel lobbyPanelLobby;
+//     private JButton join;
+//     private JButton create;
+//     private JTextField inputPlayer;
+//     private JLabel TextPlayer;
+//     private JLabel TextRoom;
+//     private JTextField inputRoom;
+    
+
+//     private void LobbyGUI() {
+       
+//         setTitle("LOBBY");
+//         setSize(500, 300);
+//         setLocationRelativeTo(null);
+//         setResizable(false);
+//         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//         lobbyPanelLobby = new JPanel(new GridLayout(3, 2));
+
+//         inputPlayer = new JTextField(10);
+//         inputRoom = new JTextField(10);
+//         join = new JButton("Join");
+//         create = new JButton("Create");
+//         TextPlayer = new JLabel(" Enter number of player : ");
+//         TextRoom = new JLabel(" Enter your NumberRoom : ");
+
+//         lobbyPanelLobby.add(TextPlayer);
+//         lobbyPanelLobby.add(inputPlayer);
+//         lobbyPanelLobby.add(TextRoom);
+//         lobbyPanelLobby.add(inputRoom);
+//         lobbyPanelLobby.add(join);
+//         lobbyPanelLobby.add(create);
+
+//         add(lobbyPanelLobby, BorderLayout.CENTER);
+
+//         join.addActionListener(this);
+//         create.addActionListener(this);
+
+//         setVisible(true); 
+//     }
+
+//     private void RoomGUI() {
+//         setTitle("Room");
+//         setSize(500, 300);
+//         setLocationRelativeTo(null);
+//         setResizable(false);
+//         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//         lobbyPanelRoom = new JPanel(new GridLayout(4, 3));
+
+//         Draw = new JButton("More card noi Ja Baby Girl!");
+//         Fold = new JButton("No more card laewJa let's fight!");
+//         Ghost = new JButton("");
+//         Start = new JButton("Start laoJa");
+//         TittleStatus = new JLabel(" Status : ");
+//         StatusLabel = new JLabel(gameStatus);
+//         TittlePoint = new JLabel(" Total Point : ");
+//         TotalPoint = new JLabel(String.valueOf(score));
+   
+
+//         lobbyPanelRoom.add(TittleStatus);
+//         lobbyPanelRoom.add(StatusLabel);
+//         lobbyPanelRoom.add(TittlePoint);
+//         lobbyPanelRoom.add(TotalPoint);
+//         lobbyPanelRoom.add(Start);
+//         lobbyPanelRoom.add(Draw);
+//         lobbyPanelRoom.add(Ghost);
+//         lobbyPanelRoom.add(Fold);
+
+//         add(lobbyPanelRoom, BorderLayout.CENTER);
+
+//         Start.addActionListener(this);
+//         Draw.addActionListener(this);
+//         Fold.addActionListener(this);
+//         setVisible(true);
+//     }
+
+//     public static void main(String[] args) throws IOException {
+
+//                 Random random = new Random();
+//                 String host = "localhost";
+//                 int lobby = 10000;
+        
+                
+        
+//                 Socket lobbySocket = new Socket(host, lobby);
+//                 System.out.println("Connected to " + host + ":" + lobby);
+        
+//                 BufferedReader in = new BufferedReader(new InputStreamReader(lobbySocket.getInputStream()));
+//                 PrintWriter out = new PrintWriter(lobbySocket.getOutputStream(), true);
+        
+//                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+//                 String userInput;
+        
+//                 while (x) {
+//                     userInput = stdIn.readLine();
+//                     if (userInput.equals("exit")) {
+//                         break;
+//                     }
+//                     //JOIN 41234
+//                     else if(userInput.startsWith("JOIN")){
+//                         int score = 0;
+//                         System.out.println("joincmd");
+//                         String[] commandParts = userInput.split(" ");
+//                         port = Integer.parseInt(commandParts[1]);
+//                         Socket game = new Socket(host, port);
+//                         BufferedReader gameIn = new BufferedReader(new InputStreamReader(game.getInputStream()));
+//                         PrintWriter gameOut = new PrintWriter(game.getOutputStream(), true);
+        
+//                         BufferedReader gameStdIn = new BufferedReader(new InputStreamReader(System.in));
+//                         String gameUserInput;
+//                         //CALL GUI
+//                         while (true) {
+        
+//                             gameUserInput = gameStdIn.readLine();
+//                             if (gameUserInput.equals("exit")) {
+//                                 break;
+//                             }
+//                             else if(gameUserInput.equals("start")){
+//                                 score += random.nextInt(11) + 1;
+//                                 score += random.nextInt(11) + 1;
+//                                 if(score == 21){
+//                                     gameOut.println("SCORE 21");
+//                                 }
+//                                 else if(score > 21){
+//                                     gameOut.println("SCORE 0");
+//                                 }
+//                             }
+//                             else if(gameUserInput.equals("more")){
+//                                 score += random.nextInt(11) + 1;
+//                             }
+        
+//                             gameOut.println(gameUserInput);
+//                             System.out.println(gameIn.readLine());
+//                         }
+                        
+//                     }
+//                     out.println(userInput);
+                    
+//                     System.out.println(in.readLine());
+                    
+//                 }
+        
+//                 in.close();
+//                 out.close();
+//                 stdIn.close();
+//                 lobbySocket.close();
+//             }
+        
+
+//     public void actionPerformed(ActionEvent e) {
+//         Random random = new Random();
+//         String playerCount = inputPlayer.getText();
+//         String roomNumber = inputRoom.getText();
+
+//         if (e.getSource() == join) {
+            
+//             GameRoom = Integer.parseInt(playerCount)*10000 + Integer.parseInt(roomNumber);
+//             System.out.println("GGame : " + GameRoom);
+//             try {
+//                 Socket game = new Socket(host, GameRoom);
+//                 System.out.println("Connected to " + host + ":" + GameRoom);
+
+//                 Rin = new BufferedReader(new InputStreamReader(game.getInputStream()));
+//                 Rout = new PrintWriter(game.getOutputStream(), true);
+//             } 
+                    
+//             catch (IOException e1) {
+//                 e1.printStackTrace();
+//             }
+//             String createCommand = "JOIN " + playerCount + roomNumber;
+//             x = true;
+//             System.out.println(x);
+//             System.out.println(GameRoom);
+//             out.println(createCommand);
+//         }
+        
+//         else if (e.getSource() == create) {
+//             playerCount = inputPlayer.getText();
+//             roomNumber = inputRoom.getText();
+//             String createCommand = "CREATE " + playerCount + " " + roomNumber;
+//             x = true;
+//             port = Integer.parseInt(playerCount+roomNumber);
+//             out.println(createCommand);
+//             System.out.println(createCommand);
+//             System.out.println("Create button clicked");
+//         }
+
+//         else if (e.getSource() == Draw) {
+//             score += random.nextInt(11) + 1;
+
+//             System.out.println("Draw Card button clicked");
+//         } 
+        
+//         else if (e.getSource() == Fold) {
+//             String foldCommand = "FOLD " + score;
+//             out.println(foldCommand);
+//             System.out.println("Fold button clicked");
+//         } 
+        
+        
+//         else if (e.getSource() == Start) {
+            
+//             score += random.nextInt(11) + 1;
+//             score += random.nextInt(11) + 1;
+
+//             if (score == 21) {
+//                 String startCommand = "SCOREX 21";
+//                 out.println(startCommand);
+//             }
+//             else{
+//                 String startCommand = "START " + score;
+//                 out.println(startCommand);
+//             }
+//             System.out.println("Start button clicked");
+//         }
+        
+//     }
+   
+// }
+
--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server {
+    
     public static void main(String[] args) {
         int lobbyPort = 10000;
         int clientCount = 1;
@@ -23,63 +25,142 @@ public class Server {
             e.printStackTrace();
         }
     }
-}
-
-class ThreadForServerMultipleClient extends Thread {
-    private Socket clientSocket;
-    private int clientNumber;
-
-    public ThreadForServerMultipleClient(Socket clientSocket, int clientNumber) {
-        this.clientSocket = clientSocket;
-        this.clientNumber = clientNumber;
-    }
-
-    @Override
-    public void run() {
-        System.out.println("Client#" + clientNumber + " connected");
-        try{
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            String inputLine;
-
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println("Client#" + clientNumber + ": " + inputLine);
-                out.println("svrc: "+inputLine);
-
-                //CREATE 4 1234
-                if(inputLine.startsWith("CREATE")){
-                    String[] commandParts = inputLine.split(" ");
-                    int roomPort = Integer.parseInt(commandParts[1])*10000 + Integer.parseInt(commandParts[2]);
-                    int limit = Integer.parseInt(commandParts[1]);
-                    ServerSocket room = new ServerSocket(roomPort, limit);
-                    while (true) {
+    
+    static class ThreadForServerMultipleClient extends Thread {
+        private Socket clientSocket;
+        private int clientNumber;
+        private static int playerNumberCounter = 1;
+    
+        public ThreadForServerMultipleClient(Socket clientSocket, int clientNumber) {
+            this.clientSocket = clientSocket;
+            this.clientNumber = clientNumber;
+        }
+    
+        @Override
+        public void run() {
+            System.out.println("Client#" + clientNumber + " connected");
+            
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                String inputLine;
+    
+                while ((inputLine = in.readLine()) != null) {
+                    System.out.println("Client#" + clientNumber + ": " + inputLine);
+                    out.println("svrc: " + inputLine);
+    
+                    // CREATE 4 1234
+                    if (inputLine.startsWith("CREATE")) {
+                        String[] commandParts = inputLine.split(" ");
+                        int roomPort = Integer.parseInt(commandParts[1]) * 10000 + Integer.parseInt(commandParts[2]);
+                        int limit = Integer.parseInt(commandParts[1]);
+                        System.out.println(clientNumber + " " + roomPort);
+                        try {
+                            ServerSocket room = new ServerSocket(roomPort, limit);
+                            List<Socket> roomClientSockets = new ArrayList<>();
+                            List<Integer> roomScores = new ArrayList<>();
+                            out.println(this.clientNumber + " CREATED " + roomPort);
+    
+                            while (true) {
                                 Socket player = room.accept();
-                                // System.out.println("client: " + clientSocket.getInetAddress().getHostName());
-                
-                                Room RoomThread = new Room(player, clientNumber);
-                                RoomThread.start();
+                                int playerNumber = playerNumberCounter++;
+                                if(playerNumber > limit){
+                                    player.close();
+                                    playerNumber = playerNumberCounter--;
+                                }
+                                else{
+                                    Room RoomThread = new Room(player, playerNumber, roomClientSockets, roomScores);
+                                    roomClientSockets.add(player);
+                                    roomScores.add(-1);
+                                    RoomThread.start();
+                                }
                             }
+                        } catch (IOException error) {
+                            error.printStackTrace();
+                        }
+                    }
                 }
+            } catch (IOException error) {
+                error.printStackTrace();
             }
-
-        }
-        catch(IOException error){
-            error.printStackTrace();
         }
     }
-}
+    static class Room extends Thread {
+        private Socket playerSocket;
+        private int playerNumber;
+        private int score;
+        private List<Socket> roomClientSockets;
+        private List<Integer> roomScores;
+    
+        public Room(Socket playerSocket, int playerNumber,List<Socket> roomClientSockets,List<Integer> roomScores) {
+            this.playerSocket = playerSocket;
+            this.playerNumber = playerNumber;
+            this.score = 0;
+            this.roomClientSockets = roomClientSockets;
+            this.roomScores = roomScores;
+        }
+    
+        @Override
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(playerSocket.getInputStream()));
+                PrintWriter out = new PrintWriter(playerSocket.getOutputStream(), true);
+                String inputLine;
+                out.println(playerNumber + " join " + playerSocket.getLocalPort());
+                while ((inputLine = in.readLine()) != null) {
+                    out.println("rrc: " + inputLine);
+                    System.out.println(playerNumber + ": " + inputLine);
 
-class Room extends Thread{
-    private Socket playerSocket;
-    private int playerNumber;
+                    // SCOREX 21 - boardcast
+                    if(inputLine.equals("SCOREX 21")){
+                        System.out.println("SOMEONE BJ!");
+                        for (Socket clientSocket : roomClientSockets) {
+                            try {
+                                PrintWriter outBC = new PrintWriter(clientSocket.getOutputStream(), true);
+                                if (clientSocket.equals(playerSocket)) {
+                                    outBC.println("STATUS win");
+                                } else {
+                                    outBC.println("STATUS lose");
+                                }
+                        
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
 
-    public Room(Socket playerSocket,int playerNumber){
-        this.playerSocket = playerSocket;
-        this.playerNumber = playerNumber;
+                    }
+
+                    if(inputLine.startsWith("SCORE")){
+                        String[] commandParts = inputLine.split(" ");
+                        this.score = Integer.parseInt(commandParts[1]);
+                        roomScores.set(playerNumber-1,Integer.parseInt(commandParts[1]));  
+                    }
+
+                    if(inputLine.startsWith("STATUS")){
+                        int max = -1;
+                        int maxi = 0;
+                        for(int i=0;i<roomClientSockets.size();i++){
+                            if(roomScores.get(i) != null && roomScores.get(i) > max){
+                                max = roomScores.get(i);
+                                maxi = i;
+                            }
+                        }
+                        for(int i=0;i<roomClientSockets.size();i++){
+                            if(roomClientSockets.get(i) != null && roomClientSockets.get(i) == roomClientSockets.get(maxi)){
+                                PrintWriter outBC = new PrintWriter(roomClientSockets.get(i).getOutputStream(), true);
+                                outBC.println("STATUS win");
+                            }
+                            else if(roomClientSockets.get(i) != null && roomClientSockets.get(i) != roomClientSockets.get(maxi)){
+                                PrintWriter outBC = new PrintWriter(roomClientSockets.get(i).getOutputStream(), true);
+                                outBC.println("STATUS lose");
+                            }
+                        }
+                    }
+                }
+            } catch (IOException error) {
+                error.printStackTrace();
+            }
+        }
     }
 
-    @Override
-    public void run() {
-        
-    }
 }
